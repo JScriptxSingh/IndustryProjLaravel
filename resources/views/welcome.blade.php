6b990dac09a61c7b93ceace9244e886d217c3daa
daAftermath <44099426+daGalileo@users.noreply.github.com>
2019-04-18 10:03:37 -0700

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,5 @@
     <div class="container">
         <div class="jumbotron">
       <h1 class="display-4">Welcome</h1>
-
-      <br />
-
-      Last build : {{ $publishedDate }}
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,7 @@
 <?php
 
-use GuzzleHttp\Client;
-
 Route::get('/', function () {
-    $client = new Client(['verify' => '../cacert.pem']);
-    $res = $client->request('GET', 'https://api.github.com/repos/gshawnr/IndustryProjLaravel/commits/master', [
-        'headers' => [
-            'Authorization' => 'Bearer ff51716b5d5440f9a78f1dcba88fdb8cf501f53e',
-            'Content-Type' => 'application/x-www-form-urlencoded'
-        ]
-    ]);
-
-    $data = json_decode($res->getBody(), true);
-
-    $date = new DateTime(substr(strval(collect($data['commit']['committer']['date'])), 2, 20));
-
-    $date->modify('-7 hour');
-
-    $formatDate = "D M, d Y";
-    $formatTime = "h i A";
-
-    $finalDate = date_format($date, $formatDate) . " at " .  date_format($date, $formatTime);
-
-    return view('welcome', [
-        'publishedDate' => $finalDate
-    ]);
+    return view('welcome');
 });
 
 Auth::routes();
