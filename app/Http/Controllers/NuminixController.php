d43201d077873034189a16807aa90043d9e4d5c3
daAftermath <44099426+daGalileo@users.noreply.github.com>
2019-04-25 14:12:48 -0700

    dont bar no 1

--- app/Http/Controllers/NuminixController.php
+++ app/Http/Controllers/NuminixController.php
@@ -16,16 +16,22 @@ class NuminixController extends Controller
 
     public function processData (Request $request) {
         $chart = new DefaultChart;
-        // $chart->labels(['One', 'Two', 'Three', 'Four']);
-        // $chart->dataset('My dataset', 'bar', [1, 2, 3, 4]);
 
         $startDateSplit = explode('-', $request->startDate);
         $startDate = $startDateSplit[0] . '-' . $startDateSplit[1] . '-01';
+        // echo $startDate . '<br>';
 
         $oneMonthAfterStart = $startDateSplit[0] . '-' . $startDateSplit[1] . '-31';
 
         $endDateSplit = explode('-', $request->endDate);
         $endDate = $endDateSplit[0] . '-' . $endDateSplit[1] . '-31';
+
+        $startDates = [];
+        $endDates = [];
+
+        // for ($year = $startDateSplit[0]; $year <= $endDateSplit[0]; $year ++) {
+        //     // for ($month = $startDateSplit)
+        // }
         
         $priorCustomers = DB::table('finaltable')
             ->where('date_purchased', '<', $startDate)
@@ -36,20 +42,33 @@ class NuminixController extends Controller
         $newCustomers = DB::table('finaltable')
             ->whereBetween('date_purchased', [$startDate, $oneMonthAfterStart])
             ->whereNotIn('cust_id', $priorCustomers)
+            ->whereNotIn('orders_status', ['5', '8'])
             ->distinct()
+            ->orderby('cust_id')
             ->pluck('cust_id')
             ->toArray();
 
         $newCustomerOrders = DB::table('finaltable')
             ->whereBetween('date_purchased', [$startDate, $endDate])
             ->whereIn('cust_id', $newCustomers)
+            ->whereNotIn('orders_status', ['5', '8'])
             ->orderby('date_purchased', 'asc')
             ->pluck('orderid')
             ->toArray();
 
+        // $orderskdjghvkd = DB::table('finaltable')
+        //     ->whereIn('orderid', $newCustomerOrders)
+        //     ->get();
+
+        // foreach ($newCustomers as $aa) {
+        //     echo $aa . '<br>';
+        // }
+
         $totalAmount = DB::table('finaltable')
             ->whereIn('orderid', $newCustomerOrders)
             ->sum('ordertotal');
+
+            echo $totalAmount;
 
         $totalTax = DB::table('finaltable')
             ->whereIn('orderid', $newCustomerOrders)
@@ -62,11 +81,6 @@ class NuminixController extends Controller
         $finalAmount = $totalAmount - ($totalTax + $totalShipping);
 
         $lifetimeValue = $finalAmount / count($newCustomers);
-
-        echo $totalAmount . '<br />';
-        echo $totalTax . '<br />';
-        echo $totalShipping . '<br />';
-        echo $finalAmount . '<br />';
 
         $chart->labels([$startDate . ' to ' . $endDate]);
         $chart->dataset('Lifetime Value', 'bar', [round($lifetimeValue, 2)]);
