e1d6004d18ab0b111380c1e87d1d628d464c10bb
daAftermath <44099426+daGalileo@users.noreply.github.com>
2019-05-07 12:00:02 -0700

    fixed end date prior to start date

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,20 @@
-$('#startDate').change( function() {
-    alert($('#startDate').value());
-    $('#endDate').attr({
-        "min" : $var
-     });    
-});
+$('#startDate').change(setMinEndDate());
+$(document).ready(setMinEndDate());
+
+function setMinEndDate() {
+    var date = new Date($('#startDate').val());
+
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+
+    if (month == 12) {
+        year += 1;
+        month = 1;
+    } else {
+        month += 1;
+    }
+
+    $('#endDate').prop(
+        "min", year + '-' + month.toString().padStart(2, '0')
+    );
+}
